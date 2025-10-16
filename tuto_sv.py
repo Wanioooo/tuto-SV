ebbabec2d8bd098f300cc6d755d8c5e37288617e
Wanioooo <syazwaniemuhtad@gmail.com>
2025-10-16 21:43:16 +0800

    Update tuto_sv.py

--- tuto_sv.py
+++ tuto_sv.py
@@ -80,8 +80,7 @@ def clean_gpa_data(data):
 
 # --- Visualization Functions (using Plotly) ---
 
-st.info(" The survey strongly shows that women are the main voice in this data, making up over three-quarters of the respondents ($\approx 76\%$). 
-Because of this, we need to remember that all the satisfaction scores and opinions we see are primarily reflecting the female students' experience in the faculty.")
+st.info(" The survey strongly shows that women are the main voice in this data, making up over three-quarters of the respondents ($\approx 76\%$). Because of this, we need to remember that all the satisfaction scores and opinions we see are primarily reflecting the female students' experience in the faculty.")
     
 
 def plot_gender_distribution(data):
